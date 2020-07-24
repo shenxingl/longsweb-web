2cca319f1258d479d02fa8d70300ffddf81cdc3b
shenxl <shen@neuqsoft.com>
2020-07-24 10:21:40 +0800

    上传文件

--- src/page/mycollect/filelist.vue
+++ src/page/mycollect/filelist.vue
@@ -1,13 +1,13 @@
 <template>
-<div>
-  <div style="width: 300px;height: 200px;border: 1px solid #909399"></div>
-  <input ref="file" type="file" @change="fileChange($event)"></input>
-  <el-button @click="upload">上传</el-button>
-</div>
+  <div>
+    <input id="file" ref="Uploader" type="file" style="display: none" @change="fileChange($event)"></input>
+    <el-button @click="uploadFile">上传</el-button>
+  </div>
 </template>
 
 <script>
 import axios from 'axios'
+
 export default {
   name: 'filelist',
   data() {
@@ -16,18 +16,43 @@ export default {
     }
   },
   methods: {
-    fileChange(event) {
-      this.file = event.target.files[0]
+
+    /**
+     * 上传按钮
+     */
+    uploadFile() {
+      document.getElementById('file').click()
     },
-    upload() {
+
+    /**
+     * 文件改变或选择时
+     */
+    fileChange(event) {
+      this.$confirm('当前选择文件：[' + event.target.files[0].name + ']，是否上传？', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+      }).then(() => {
+        this.file = event.target.files[0]
+        this.doUpload()
+      }).catch(() => {
+        this.cleanData()
+      })
+    },
+
+    /**
+     * 上传文件
+     */
+    doUpload() {
       let formData = new FormData()
       formData.append('file', this.file)
       axios.post('/filectrl/upload', formData).then((res) => {
         if (res.data.code === 0) {
           let fileInfo = res.data.fileInfo
           this.$confirm('上传成功。上传图片：' + fileInfo.fileName + '。存储路径：' + fileInfo.filePath, '提示', {
-            confirmButtonText: '确定'
+            confirmButtonText: '确定',
+            cancelButtonText: '取消',
           })
+          this.cleanData()
         } else {
           this.$message.error(res.data.msg)
         }
@@ -35,6 +60,14 @@ export default {
         this.$message.error('服务异常')
       })
     },
+
+    /**
+     * 清空
+     */
+    cleanData() {
+      this.$refs.Uploader.value = ''
+      this.file = ''
+    }
   }
 }
 </script>
